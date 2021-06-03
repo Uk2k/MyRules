471c9b50619e44e23c694d4e2699650f8117c718
sdonnachie-ttc <71264697+sdonnachie-ttc@users.noreply.github.com>
2021-06-04 00:53:17 +0100

    Learning to Ski rule with failing tests

---
+++ MyRules/MyRules.Business/OrderRules/LearningToSkiOrderRule.cs
@@ -0,0 +1,22 @@
+﻿namespace MyRules.Business.OrderRules
+{
+    using System;
+    using System.Threading.Tasks;
+    using Contracts;
+    using PackingSlips;
+
+    public class LearningToSkiOrderRule : IOrderRule
+    {
+        private readonly IPackingSlipHandler _packingSlipHandler;
+
+        public LearningToSkiOrderRule(IPackingSlipHandler packingSlipHandler)
+        {
+            _packingSlipHandler = packingSlipHandler;
+        }
+
+        public async Task CheckRule(IOrder order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyRules/MyRules.BusinessTests/OrderRules/LearningToSkiOrderRuleTests.cs
@@ -0,0 +1,51 @@
+﻿namespace MyRules.BusinessTests.OrderRules
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Business.OrderRules;
+    using Business.PackingSlips;
+    using Contracts;
+    using Contracts.Products;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    public class LearningToSkiOrderRuleTests
+    {
+        private IPackingSlipHandler packingSlipHandler;
+        private LearningToSkiOrderRule ruleUnderTest;
+        private Product product;
+        private Order order;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.packingSlipHandler = Substitute.For<IPackingSlipHandler>();
+
+            this.ruleUnderTest = new LearningToSkiOrderRule(this.packingSlipHandler);
+        }
+
+        [Test]
+        public async Task ShouldCreatePackingSlip()
+        {
+            var book = new PhysicalBook("Learning To Ski");
+
+            this.order = new Order("", 1, book);
+
+            await this.ruleUnderTest.CheckRule(this.order);
+
+            await this.packingSlipHandler.Received().CreatePackingSlipWithReason(book, "the result of a court decision in 1997).");
+        }
+
+        [Test]
+        public async Task ShouldNotCreatePackingSlip()
+        {
+            var book = new PhysicalBook("Learning to sow");
+
+            this.order = new Order("", 1, product);
+
+            await this.ruleUnderTest.CheckRule(this.order);
+
+            await this.packingSlipHandler.DidNotReceive().CreatePackingSlipWithReason(book, "the result of a court decision in 1997).");
+        }
+    }
+}
